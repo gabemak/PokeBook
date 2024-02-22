ac0ba05ca796ac84a91cf69d075968b4c438a3ed
Makay44 <88712262+Makay44@users.noreply.github.com>
2024-02-22 17:51:51 +0100

    basic titles, intialized project folder

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>PokéBook</h1>
+      <h2>Who said, that pokémons doesn't need a photobook? :-)</h2>
+
+      <h3>Pokémons are tricky animals. Sometimes it is really hard to notice them</h3>
+
+      <h3>In other times you need to focus a little bit to see the essence</h3>
+
+      <h3>They say: step back to see the whole picture</h3>
+
+      <h3>And maybe there are pictures you don not wan to remember</h3>
+    </>
+  );
+}
+
+export default App;
