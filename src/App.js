378bb6dbf8d7b29e69ec1cd3fe4f0699ba2a08c9
Makay44 <88712262+Makay44@users.noreply.github.com>
2024-03-28 20:10:55 +0100

    Title section ready, modified result card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,122 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    img1: "",
+    img2: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  })
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      setPokemon({
+        img1: response.data.sprites.other.home.front_default,
+        img2: response.data.sprites.other.home.front_shiny,
+        species: response.data.species.name,
+        img: response.data.sprites.other.dream_world.front_default,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name,
+      });
+      setPokemonChosen(true);
+    })
+  }
+
   return (
     <>
-      <h1>PokéBook</h1>
-      <h2>Who said, that pokémons doesn't need a photobook? :-)</h2>
+      <div className='App'>
 
-      <h3>Pokémons are tricky animals. Sometimes it is really hard to notice them</h3>
+        <div className='WelcomeSection'>
+          <div className="wrapper">
+            <div className="wrapperten">
+              <div>
+                <h1>Welcome to</h1>
+              </div>
+              <div>
+                <h3 className="bounce">
+                  <span>P</span>
+                  <span>O</span>
+                  <span>K</span>
+                  <span>E</span>
+                  <span>B</span>
+                  <span>O</span>
+                  <span>O</span>
+                  <span>K</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <h3>In other times you need to focus a little bit to see the essence</h3>
+        <div className='TitleSection'>
+          <h1>Pokemon stats</h1>
+          <input type='text' onChange={(event) => { setPokemonName(event.target.value) }}></input>
+          <button onClick={searchPokemon}>Search Pokemon</button>
+          <h5>Please type the name of a Pokemon and click on "Search Pokemon" button</h5>
+          <div>
+            <p className="marquee"><span>Pokemons to search: bulbasaur - charmander - pikachu - mew - butterfree - pidgeot - nidorino - wigglytuff - venomoth - poliwag - tentacool - slowbro - doduo - onix - and so much more :-D</span></p>
+          </div>
+        </div>
 
-      <h3>They say: step back to see the whole picture</h3>
+        <div className='DisplaySection'>
+          {!pokemonChosen ? (<h1>No Pokemon chosen</h1>) : (
+            <div className='ResultLine'>
+              <div className='DisplaySectionImgLeft'>
+                <img src={pokemon.img1} />
+              </div>
+              <div className='ResultCardBackground'>
+                <div className='ResultCard'>
+                  <div className='ResultCardTop'>
+                    <img src={pokemon.img} />
+                  </div>
+                  <h3>Species: {pokemon.species}</h3>
+                  <h3>Type: {pokemon.type}</h3>
+                  <h3>HP: {pokemon.hp}</h3>
+                  <h3>Attack: {pokemon.attack}</h3>
+                  <h3>Defense: {pokemon.defense}</h3>
+                </div>
+              </div>
+              <div className='DisplaySectionImgRight'>
+                <img src={pokemon.img2} />
+              </div>
+            </div >
+          )
+          }
+          {pokemon.species === "" ? (console.log('No such Pokemon in the database, try another one')) : (pokemon.species)}
 
-      <h3>And maybe there are pictures you don not wan to remember</h3>
+
+          <div className='StrongestPokemons'>
+            <h2>Strongest Pokemons:</h2>
+            {pokemon.attack > 95 ? (<h1>No Pokemon chosen</h1>) : (
+              <div className='Result'>
+                <img src={pokemon.img} />
+                <h3>Species: {pokemon.species}</h3>
+                <h3>Type: {pokemon.type}</h3>
+                <h3>HP: {pokemon.hp}</h3>
+                <h3>Attack: {pokemon.attack}</h3>
+                <h3>Defense: {pokemon.defense}</h3>
+              </div>
+            )}
+          </div>
+
+        </div >
+
+      </div >
     </>
   );
+  console.log(pokemon.species)
 }
 
 export default App;
